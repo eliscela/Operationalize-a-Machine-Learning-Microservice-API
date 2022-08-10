f321775e91adced71163353380ec9af168af05ad
Elis Cela <24852039+eliscela@users.noreply.github.com>
2022-08-10 21:55:27 +0000

    Add kubernetes run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="tahirsenpai/price-predictor:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run price-predictor --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward price-predictor 8000:80
